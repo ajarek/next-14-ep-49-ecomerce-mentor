5d22fe9136784e56ce4763f36f1a5e8dfcd6b916
Ajarek <ajarek@poczta.onet.pl>
2024-07-18 00:36:27 +0200

    added list

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,22 @@
 import Image from "next/image";
-
+import data from '@/data/data.json'
 export default function Home() {
   return (
     <main className="flex bg-[var(--Rose-300)] min-h-screen flex-col items-center justify-between p-24">
-      <h1>lol</h1>
+      <div className='grid grid-cols-3 gap-4'>
+
+      {data.map((item, index) => (
+        <div key={index}>
+          <Image src={item.image.thumbnail} alt={item.name} width={300} height={300} />
+          <p>{item.name}</p>
+          <p>{item.category}</p>
+          <p>{(item.price).toFixed(2)}$</p>
+        </div>
+      ))}
+
+
+
+      </div>
     </main>
   );
 }
